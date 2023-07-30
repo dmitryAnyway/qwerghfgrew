a97042b110710a919c1afc0b2092261093662a81
ext.dzubarev <ext.dzubarev@tinkoff.ru>
2023-07-30 19:34:10 +0300

    Initial Commit

---
+++ qwerghfgrew/qwerghfgrew.h
@@ -0,0 +1,18 @@
+//
+//  qwerghfgrew.h
+//  qwerghfgrew
+//
+//  Created by Дмитрий on 30.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for qwerghfgrew.
+FOUNDATION_EXPORT double qwerghfgrewVersionNumber;
+
+//! Project version string for qwerghfgrew.
+FOUNDATION_EXPORT const unsigned char qwerghfgrewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <qwerghfgrew/PublicHeader.h>
+
+
